5b29c9aba014a766f88c1d2526794a3bb2bc1494
Morten Hjortshøj Nielsen <mhn@assemble.dk>
2019-12-10 13:50:26 +0100

    NI-877

---
+++ THPinViewController/THLocalStringManager.h
@@ -0,0 +1,17 @@
+//
+//  THLocalStringManager.h
+//  THPinViewControllerExample
+//
+//  Created by Morten Hjortshøj Nielsen on 10/12/2019.
+//  Copyright © 2019 Thomas Heß. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface THLocalStringManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ THPinViewController/THLocalStringManager.m
@@ -0,0 +1,13 @@
+//
+//  THLocalStringManager.m
+//  THPinViewControllerExample
+//
+//  Created by Morten Hjortshøj Nielsen on 10/12/2019.
+//  Copyright © 2019 Thomas Heß. All rights reserved.
+//
+
+#import "THLocalStringManager.h"
+
+@implementation THLocalStringManager
+
+@end
